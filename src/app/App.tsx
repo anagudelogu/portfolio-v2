474dc9ba530d691601a6dab42cdcc9d2b1c79d56
andres <anagudelogu@gmail.com>
2023-04-09 11:04:23 -0500

    Install and configure react-daisy

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,13 @@
 import { memo, type FC } from 'react'
+import { Button } from 'react-daisyui'
 
 export const App: FC = memo(() => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Button color="primary">Click Me!</Button>
+    </div>
+  )
 })
 
 App.displayName = 'App'
