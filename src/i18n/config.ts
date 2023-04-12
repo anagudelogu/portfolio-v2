0b5591963ae7643d992dce55325078a08c37c811
andres <anagudelogu@gmail.com>
2023-04-11 20:46:34 -0500

    Add language type and add ddummy spanish translations

--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -2,10 +2,14 @@ import i18next from 'i18next'
 import { initReactI18next } from 'react-i18next'
 import LanguageDetector from 'i18next-browser-languagedetector'
 import enTranslation from './en/translation.json'
+import esTranslation from './es/translation.json'
 
 export const resources = {
   en: {
     translation: enTranslation,
+  },
+  es: {
+    translation: esTranslation,
   },
 }
 
