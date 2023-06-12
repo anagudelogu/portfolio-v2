0c0b828ec15bb3f3a1363d45986d55fab31ed819
andres <anagudelogu@gmail.com>
2023-06-12 18:39:29 -0500

    Update translations

--- src/features/Hero/Hero.tsx
+++ src/features/Hero/Hero.tsx
@@ -1,8 +1,10 @@
 import { memo, type FC } from 'react'
 import heroImageSrc from '@/assets/hero.png'
 import { Avatar } from 'react-daisyui'
+import { Trans, useTranslation } from 'react-i18next'
 
 export const Hero: FC = memo(() => {
+  const { t } = useTranslation()
   return (
     <section className="min-h-[calc(100vh-64px)] w-full flex flex-col justify-center items-center gap-5 lg:flex-row-reverse lg:justify-between">
       <Avatar
@@ -16,16 +18,17 @@ export const Hero: FC = memo(() => {
       </Avatar>
       <div className="flex flex-col gap-2 w-full xl:w-1/2">
         <h1 className="flex flex-col text-center lg:text-start leading-none">
-          <span className="text-xl md:text-2xl font-light">
-            Hi, I&apos;m Andres
-          </span>
-          <span className="text-5xl font-extrabold lg:text-8xl">
-            FRONTEND DEVELOPER
-          </span>
+          <Trans i18nKey={'hero.title'}>
+            <span className="text-xl md:text-2xl font-light">
+              Hi, I&apos;m Andres
+            </span>
+            <span className="text-5xl font-extrabold lg:text-8xl">
+              FRONTEND DEVELOPER
+            </span>
+          </Trans>
         </h1>
         <p className="text-xl text-center md:text-2xl font-light lg:text-start">
-          Detail-oriented and team-focused professional committed to
-          transforming product ideas into outstanding user experiences.
+          {t('hero.description')}
         </p>
       </div>
     </section>
